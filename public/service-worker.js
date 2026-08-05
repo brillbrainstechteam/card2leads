8e3a5e90756a307a009f18e29a64738cdea7bf85
BBTechTeam <tech@brillbrainsconsultants.com>
2026-08-05 22:33:44 +0530

    Remove hero illustration tint

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "smartscan-shell-v29";
-const SHELL_ASSETS = ["/", "/index.html", "/styles.css?v=20260805-11", "/app.js?v=20260805-11", "/manifest.webmanifest"];
+const CACHE_NAME = "smartscan-shell-v30";
+const SHELL_ASSETS = ["/", "/index.html", "/styles.css?v=20260805-12", "/app.js?v=20260805-12", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_ASSETS)));
