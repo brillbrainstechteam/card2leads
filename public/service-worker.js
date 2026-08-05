16332d70aaf16dc96fd36d2418307ba8dc5f33d5
BBTechTeam <tech@brillbrainsconsultants.com>
2026-08-05 22:43:11 +0530

    Use soft blue feature cards

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "smartscan-shell-v31";
-const SHELL_ASSETS = ["/", "/index.html", "/styles.css?v=20260805-13", "/app.js?v=20260805-13", "/manifest.webmanifest"];
+const CACHE_NAME = "smartscan-shell-v32";
+const SHELL_ASSETS = ["/", "/index.html", "/styles.css?v=20260805-14", "/app.js?v=20260805-14", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_ASSETS)));
