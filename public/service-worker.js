c833456c46338794f1c4e84ab95d7202ec4d5bc8
BBTechTeam <tech@brillbrainsconsultants.com>
2026-08-05 22:18:21 +0530

    Enlarge homepage hero illustration

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "smartscan-shell-v28";
-const SHELL_ASSETS = ["/", "/index.html", "/styles.css?v=20260805-10", "/app.js?v=20260805-10", "/manifest.webmanifest"];
+const CACHE_NAME = "smartscan-shell-v29";
+const SHELL_ASSETS = ["/", "/index.html", "/styles.css?v=20260805-11", "/app.js?v=20260805-11", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_ASSETS)));
